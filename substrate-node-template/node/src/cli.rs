2d3f7c3222364b9460ce20560db8a2643aeda5b2
wangzhenchuan <191623431@qq.com>
2024-02-19 16:20:43 +0800

    lesson2

--- substrate-node-template/node/src/cli.rs
+++ substrate-node-template/node/src/cli.rs
@@ -41,9 +41,12 @@ pub enum Subcommand {
 	#[command(subcommand)]
 	Benchmark(frame_benchmarking_cli::BenchmarkCmd),
 
-	/// Try-runtime has migrated to a standalone CLI
-	/// (<https://github.com/paritytech/try-runtime-cli>). The subcommand exists as a stub and
-	/// deprecation notice. It will be removed entirely some time after Janurary 2024.
+	/// Try some command against runtime state.
+	#[cfg(feature = "try-runtime")]
+	TryRuntime(try_runtime_cli::TryRuntimeCmd),
+
+	/// Try some command against runtime state. Note: `try-runtime` feature must be enabled.
+	#[cfg(not(feature = "try-runtime"))]
 	TryRuntime,
 
 	/// Db meta columns information.
